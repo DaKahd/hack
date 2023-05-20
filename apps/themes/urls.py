d5601a262e76a17b558d4f91d5d793389c962b23
gitwarrio2 <112347680+gitwarrio2@users.noreply.github.com>
2023-05-20 15:47:31 -0700

    ff

--- apps/themes/urls.py
+++ apps/themes/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
+    path('', views.all_themes_index),
     path('shop-by-category/', views.all_themes),
     path('shop-by-category/<slug:slug>/', views.theme_posts)
     
