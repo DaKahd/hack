31340707065bf5c024de402f96edb9b5640a4972
DaKahd <90017411+DaKahd@users.noreply.github.com>
2023-05-20 16:51:29 -0700

    Update apps.py

--- apps/adminModel/apps.py
+++ apps/adminModel/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AdminmodelConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.adminModel'
+    path='/home/davidk/hack/apps/adminModel'
