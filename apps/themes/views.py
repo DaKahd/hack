0c0baf32580f121f6490a98503e01c63c43c63a5
gitwarrio2 <112347680+gitwarrio2@users.noreply.github.com>
2023-05-20 17:29:57 -0700

    FEFrsrdrrrrggregsgsf

--- apps/themes/views.py
+++ apps/themes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from apps.themes.models import ParentTheme, ChildTheme
 from apps.products.models import Product
+from apps.products.forms import ProductSearchForm, ProductSortForm
 
 
 def all_themes_index(request):
@@ -24,5 +25,23 @@ def theme_posts(request, slug):
     child_theme = get_object_or_404(ChildTheme, slug=slug)
     products = Product.objects.filter(child_theme=child_theme)
 
-    return render(request, 'themes/theme_post.html', {'child_theme': child_theme, 'products': products})
+    search_form = ProductSearchForm(request.GET)
+    sort_form = ProductSortForm(request.GET)
+
+    
+    if search_form.is_valid():
+        search_query = search_form.cleaned_data['search_query']
+        products = products.filter(title__icontains=search_query)
+
+    if sort_form.is_valid():
+        sort_option = sort_form.cleaned_data['sort_option']
+        if sort_option == 'lowest':
+            products = products.order_by('price')
+        elif sort_option == 'highest':
+            products = products.order_by('-price')
+        elif sort_option == 'sale':
+            products = products.filter(sale=True)
+
+    return render(request, 'themes/theme_post.html', {'child_theme': child_theme, 'products': products, 
+                                                      'search_form': search_form, 'sort_form': sort_form})
 
